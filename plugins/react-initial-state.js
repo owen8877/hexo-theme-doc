6e330faae0516087a8d37165f03567d658bddbbb
owen8877 <owentoget@yeah.net>
2019-09-28 19:52:32 -0500

    Simplified navigation.yaml.

--- plugins/react-initial-state.js
+++ plugins/react-initial-state.js
@@ -26,9 +26,29 @@ module.exports = ({hexo}) => {
       }, {});
   };
 
+  const recursive_build = o => {
+    return o.map(item => {
+      if (typeof item === "object") {
+        let key = Object.keys(item)[0]
+        let value = item[key]
+          if (typeof value === "object") // A label with children
+            return {text: key, type: "label", children: recursive_build(value)}
+          else // A link
+            return {text: key, type: "link", path: value}
+      } else // A label with no children
+        return {text: item, type: "label"}
+    })
+  }
+
+  const build_navigation = n => {
+    return n.build ? n : {logo: {text: n.logo, type: "link", path: "index.html"}, main: recursive_build(n.main), build: true}
+  }
+
   hexo.extend.filter.register('template_locals', function (locals){
 
-    const data = locals.site.data;
+    let data = locals.site.data;
+
+    data.navigation = build_navigation(data.navigation);
 
     const page = filter(locals.page, allowedProperties.page);
 
